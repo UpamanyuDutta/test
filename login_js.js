3a2cdd8d551c76949dad59b2c246e8a94db11589
UpamanyuDutta <92217498+UpamanyuDutta@users.noreply.github.com>
2024-12-01 21:56:50 +0530

    Commit Upam

--- login_js.js
+++ login_js.js
@@ -1,12 +1,12 @@
-// script.js
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-  
+
     if (!username || !password) {
-      alert("Please fill in both fields.");
-      event.preventDefault(); // Prevent form submission
+        alert("Please fill in both fields.");
+        event.preventDefault(); // Prevent form submission
+    } else {
+        // Only redirect if both fields are filled
+        window.location.href = 'p2.html';
     }
-    window.location.href = 'p2.html';
-  });
-  
+});
