c2e799b2ccd658fbc4ffb0b9560d7aaa153a413b
UpamanyuDutta <92217498+UpamanyuDutta@users.noreply.github.com>
2024-12-01 19:11:39 +0530

    Commiting Day 1 Upamanyu

---
+++ Pages/login_js.js
@@ -0,0 +1,11 @@
+// script.js
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    if (!username || !password) {
+      alert("Please fill in both fields.");
+      event.preventDefault(); // Prevent form submission
+    }
+  });
+  
